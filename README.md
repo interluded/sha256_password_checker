a3e82bf519a46e8bbe9d433c36775f3eed976f0a
marclmao <119444616+marclmao@users.noreply.github.com>
2023-11-17 11:10:23 -0500

    looped

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # sha256_password_checker
 build requirements:
 apache commons codec >=1.16 (older may work, haven't tried)
-lol
--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int tries = 0;
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter The Password: ");
         String password_unencrypted = scan.nextLine();
-        String encrypted_still = ("0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e");
+        String encrypted_still = ("9fe93cfaab2c64f30c037620abc1bc0ac69da0464b22900086d4a77982772c30");
         String encrypted_dynamic = org.apache.commons.codec.digest.DigestUtils.sha256Hex(password_unencrypted);
+        if (encrypted_dynamic.equals("9fe93cfaab2c64f30c037620abc1bc0ac69da0464b22900086d4a77982772c30")){
+            System.out.println("Password Correct");
+        }
+        tries++;
 
-        while (!encrypted_still.equals(encrypted_dynamic)) {
+
+        while (!encrypted_still.equals(encrypted_dynamic)){
             System.out.println("Incorrect,");
             System.out.println("Enter The Password: ");
             password_unencrypted = scan.nextLine();
-            encrypted_still = ("0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e");
+            encrypted_still = ("9fe93cfaab2c64f30c037620abc1bc0ac69da0464b22900086d4a77982772c30");
             encrypted_dynamic = org.apache.commons.codec.digest.DigestUtils.sha256Hex(password_unencrypted);
+            tries++;
+            if (encrypted_dynamic.equals("9fe93cfaab2c64f30c037620abc1bc0ac69da0464b22900086d4a77982772c30")){
+                System.out.println("Password Correct");
+            }
+            if (tries > 3){
+                System.out.println("You have been locked out");
+                System.exit(0);
+            }
+
         }
 
-        System.out.println("Password Correct");
+
     }
 }
